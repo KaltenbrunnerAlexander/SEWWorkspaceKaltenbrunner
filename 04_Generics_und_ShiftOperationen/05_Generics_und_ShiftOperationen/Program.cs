33652c301707d2ed6158ef6b5be845859e94b2e2
AlexanderKaltenbrunner <alexanderkaltenbrunner6@gmail.com>
2025-10-09 15:46:55 +0200

    All commited

---
+++ 04_Generics_und_ShiftOperationen/05_Generics_und_ShiftOperationen/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Generics_und_ShiftOperationen
+{
+    
+    class Node<T>
+    {
+        public T Value;
+        public Node<T> next;
+        public Node(T value)
+        {
+            Value = value;
+        }
+    }
+    class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public void Add(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        public void Shift(int x)
+        {
+            Node<T> newStart = head;
+            for(int i = 0; i < x; i++)
+            {
+                if (newStart != null)
+                    newStart = newStart.next;
+            }
+
+            Node<T> oldStart = head;
+            head = newStart;
+
+            
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            Node<T> current = head;
+            while (current != null)
+            {
+                sb.Append(current.Value + " ");
+                current = current.next;
+            }
+            return sb.ToString();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            LinkedList<string> list = new LinkedList<string>();
+
+            list.Add("A");
+            list.Add("B");
+            list.Add("C");
+            list.Add("D");
+            list.Add("E");
+
+            Console.WriteLine("Liste bevor Shift: ");
+            
+        }
+    }
+}
---
+++ 05_Formatted_Logger/05_Formatted_Logger/05_Formatted_Logger/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using System.Net.Configuration;
+
+namespace _05_Formatted_Logger
+{
+    internal class Program
+    {
+        private static int maxLength = 0;
+        public static string FormatAddDateTime(string s)
+        {
+            return $"{DateTime.Now}: {s}";
+        }
+
+        public static string FormatUpperCase(string s)
+        {
+            return s.ToUpper();
+        }
+
+        public static void SetLengthLimit(int length)
+        {
+            maxLength = length;
+        }
+
+        public static string FormatLengthLimited(string s)
+        {
+            return s.Length > maxLength ? s.Substring(0, maxLength) + "..." : s;
+        }
+
+        static void Main(string[] args)
+        {
+            Logging logEntry = new Logging();
+
+            
+            logEntry.AddLogMessage("System not found");
+            logEntry.AddLogMessage("User not found");
+            logEntry.AddLogMessage("File not found");
+
+            Console.WriteLine("Original");
+            logEntry.PrintMessage(null);
+
+            Console.WriteLine("\nFormat with DateTime");
+            logEntry.PrintMessage(FormatAddDateTime);
+
+            Console.WriteLine("\nFormat with UpperCase");
+            logEntry.PrintMessage(FormatUpperCase);
+
+            Console.WriteLine("\nFormat with a Length Limit");
+            SetLengthLimit(30);
+            logEntry.PrintMessage(FormatLengthLimited);
+
+            Console.ReadKey();
+
+        }
+    }
+    class Logging
+    {
+        private List<string> logMessages = new List<string>();
+        public void AddLogMessage(string msg)
+        {
+            logMessages.Add(msg);
+        }
+
+        public void PrintMessage(Func<string, string> formattingFunc)
+        {
+            foreach (var msg in logMessages)
+            {
+                string formatted;
+
+                if (formattingFunc != null)
+                    formatted = formattingFunc(msg);
+                else
+                    formatted = msg;
+
+                Console.WriteLine(formatted);
+            }
+        }
+
+        
+    }
+}
