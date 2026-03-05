ca509ad685379ba1d36d966b9553c259c94f8a4c
AlexanderKaltenbrunner <alexanderkaltenbrunner6@gmail.com>
2026-03-05 15:36:54 +0100

    20_MVVM added

---
+++ 20_MVVM/20_MVVM/NumberViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_MVVM
+{
+    internal class NumberViewModel
+    {
+        private NumberModel model = new NumberModel();
+        public int Number 
+        {
+            get
+            {
+                return model.Number;
+            }
+            set
+            {
+                model.Number = value;
+            }
+            
+        }
+
+    }
+}
---
+++ 20_MVVM/TimerExample/TimedNumberViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Windows.Input;
+using System.IO;
+
+namespace TimerExample
+{
+    internal class TimedNumberViewModel : INotifyPropertyChanged, ICommand
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        Random random = new Random();
+
+        private TimedNumberModel model = new TimedNumberModel();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler CanExecuteChanged;
+        public ICommand CmdButtonClick { get; set; }
+        public int Number {
+            get { return model.Number; }
+            set {
+                model.Number = value;
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Number"));
+            }
+        }
+
+        public void UpdateNumber(object sender, EventArgs args)
+        {
+            Number = random.Next(0, 1000);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if ((string)parameter == "value0")
+                Number = 0;
+            else if ((string)parameter=="value1000")
+                Number = 1000;
+        }
+
+
+        public TimedNumberViewModel()
+        {
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += UpdateNumber;
+            timer.Start();
+
+            CmdButtonClick = this;
+        }
+
+
+    }
+}
