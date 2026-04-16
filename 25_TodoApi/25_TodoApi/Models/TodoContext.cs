c3e905cfa26b851bf005ce9715152a36a6f692dc
AlexanderKaltenbrunner <alexanderkaltenbrunner6@gmail.com>
2026-04-16 14:03:14 +0200

    MyRelayCommand, TodoAPI and CryptoPriceAPI added

---
+++ 25_TodoApi/25_TodoApi/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _25_TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
